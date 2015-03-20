a8ea4fe86b51c76966298afae6346ee4f0fedb1a
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-03-19 23:14:05 -0400

    hardcoded processes

--- config/docker/unicorn.rb
+++ config/docker/unicorn.rb
@@ -8,7 +8,7 @@ pid "#{app_dir}/tmp/unicorn.pid"
 stderr_path "#{app_dir}/log/unicorn.stderr.log"
 stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
-worker_processes ENV["SYSOPIA_UNICORN_WORKERS"].to_i
+worker_processes 2
 listen 8080, :tcp_nopush => true
 timeout 30
 

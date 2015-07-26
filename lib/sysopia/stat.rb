a83bc49528a7d67b02f42221a32c458039d77ef4
Viduranga Wijesooriya <vpowerrc@gmail.com>
2015-07-26 13:59:14 +0530

    updated graph title
    Signed-off-by: Viduranga Wijesooriya <vpowerrc@gmail.com>

--- lib/sysopia/stat.rb
+++ lib/sysopia/stat.rb
@@ -2,77 +2,77 @@ module Sysopia
   class Stat
     DATA = {
       load_one: {
-        description: "Average Load 1min",
+        description: "Average Load 1 Min",
         unit: "%",
         multiplier: 100
       },
       load_five: {
-        description: "Average Load 5min",
+        description: "Average Load 5 Min",
         unit: "%",
         multiplier: 100
       },
       load_fifteen: {
-        description: "Average Load 15min",
+        description: "Average Load 15 Min",
         unit: "%",
         multiplier: 100
       },
       cpu_user: {
-        description: "User time on cpu",
+        description: "User Time on CPU",
         unit: "%",
         multiplier: 100
       },
       cpu_system: {
-        description: "System time on cpu",
+        description: "System Time on CPU",
         unit: "%",
         multiplier: 100
       },
       cpu_busy: {
-        description: "Percent of time cpu is active",
+        description: "Percent of Time CPU is Active",
         unit: "%",
         multiplier: 100
       },
       cpu_wait: {
-        description: "Percent of time cpu waits for IO",
+        description: "Percent of Time CPU Waits for IO",
         unit: "%",
         multiplier: 100
       },
       io_read: {
-        description: "Reads from disk to memory",
+        description: "Reads from Disk to Memory",
         unit: "MB/sec",
         multiplier: 1
       },
       io_write: {
-        description: "Write to disk from memory",
+        description: "Write to Disk from Memory",
         unit: "MB/sec",
         multiplier: 1
       },
       swap_used: {
-        description: "Amount of swap memory engaged",
+        description: "Amount of Swap Memory Engaged",
         unit: "MB",
         multiplier: 1
       },
       swap_in: {
-        description: "Memory moved to swap",
+        description: "Memory Moved to Swap",
         unit: "MB",
         multiplier: 1
       },
       swap_out: {
-        description: "Memory moved from swap",
+        description: "Memory Moved from Swap",
         unit: "MB",
         multiplier: 1
       },
       memory_taken: {
-        description: "Percent of used memory",
+        description: "Percent of Used Memory",
         unit: "%",
         multiplier: 1
       },
       processes_waiting: {
-        description: "Number of processes waiting in queue",
+        description: "Number of Processes Waiting in Queue",
         unit: nil,
         multiplier: 1
       },
       processes_iowaiting: {
-        description: "Number of processes locked in IO wait",
+        description: "Number of Processes Locked in IO Wait",
         unit: nil,
         multiplier: 1
       }

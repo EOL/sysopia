ee9f6a21350f75707b29d992cc59a7176625acef
Viduranga Wijesooriya <vpowerrc@gmail.com>
2015-08-13 16:03:09 +0530

    added a function to remove 1 year old stat data
    Signed-off-by: Viduranga Wijesooriya <vpowerrc@gmail.com>

--- models/today.rb
+++ models/today.rb
@@ -2,13 +2,24 @@ class Today < ActiveRecord::Base
   self.table_name = :today
 
   def self.today
-    day = self.first || Today.create(today: Sysopia.today)
-    day.today
+    day = self.first || Today.create(today: Sysopia.today)     
   end
 
-  def self.new_today
+  def self.new_today?
+    day = self.today    
+    if day.today != Sysopia.today    
+      self.save_today 
+      return true
+    else
+      return false
+    end
+  end
+
+  def self.save_today 
     day = self.today
     day.today = Sysopia.today
     day.save
+    day   
   end
+
 end
